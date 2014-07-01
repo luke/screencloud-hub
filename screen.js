51ae347109971afef33ece85537ea3e1350c34af
Nattapong kongmun <nattapong.kongmun@gmail.com>
2014-07-01 19:14:39 +0700

    add more command for test

--- screen.js
+++ screen.js
@@ -6,7 +6,7 @@ var homeId = '00000000-0000-0000-0000-000000000000';
 var appId = '765FFE19'; 
 var appNS = 'urn:x-cast:io.screencloud.cast.player';
 
-var dump = function(obj){return util.inspect(obj, false, null); };
+var dump = function(obj){ return util.inspect(obj, false, null); };
 
 var devices = {};
 
@@ -19,6 +19,7 @@ function getDeviceInfo(service)
   info.ip = service.addresses[0];
   info.port = service.port;
   info.name = service.name;
+  info.service = service;
   return info;
 }
 
@@ -35,39 +36,58 @@ function scanDevice()
 
     console.log('found device %s at %s:%d', service.name, ip, service.port);
 
-    //onDeviceUp(ip);
     browser.stop();
 
-    // console.log(devices);
+    // console.log(require('util').inspect(devices, true, 10)); 
   });
 
   browser.start();
 }
 
-function startApp(host) {
+// appId: CC1AD845 | YouTube
+// appId: 765FFE19 | ScreenCould
+function launch(host, appId, no_sleep) {
 
   var client = new Client();
   client.connect(host, function() {
-    // create various namespace handlers
+
     var connection = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.tp.connection', 'JSON');
-    // var heartbeat  = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.tp.heartbeat', 'JSON');
     var receiver   = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.receiver', 'JSON');
 
     // establish virtual connection to the receiver
     connection.send({ type: 'CONNECT' });
 
+    // start heartbeating
+    if (no_sleep) {
+      var heartbeat  = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.tp.heartbeat', 'JSON');
+      setInterval(function() {
+        heartbeat.send({ type: 'PING' });
+      }, 5000);
+    }
+
     // launch app
-    // receiver.send({ type: 'LAUNCH', appId: 'CC1AD845', requestId: 1});
-    receiver.send({ type: 'LAUNCH', appId: '765FFE19', requestId: 1});
+    
+    // receiver.send({ type: 'SET_VOLUME', volume: { level: 1 }, requestId: 11111 });
+    // receiver.send({ type: 'SET_VOLUME', volume: { muted: true }, requestId: 22222 });
+
+    // receiver.send({ type: 'LAUNCH', appId: appId, requestId: 111});
+    receiver.send({ type: 'STOP', sessionId: 'CA3CF588-062E-7F18-CCC7-A376365787EF', requestId: 222});
+
+    // receiver.send({ type: 'GET_STATUS', requestId: 555});
+    // receiver.send({ type: 'GET_APP_AVAILABILITY', appId: [ appId ], requestId: 666});
+    
+
+    
 
     //display receiver status updates
     receiver.on('message', function(data, broadcast) {
 
-      //console.log(data.status);
+      // console.log(data);
+      console.log(require('util').inspect(data, true, 10));
 
-      if(data.type = 'RECEIVER_STATUS') {
-        console.log(data.status);
-      }
+      // if(data.type = 'RECEIVER_STATUS') {
+      //   console.log(data.status);
+      // }
     });
 
   });
@@ -75,11 +95,14 @@ function startApp(host) {
 }
 
 scanDevice();
+launch('192.168.10.24', '765FFE19', true);
 
-var ipTarget = '192.168.10.24'; // for test chromecast one
-// var ipTarget = '192.168.10.46'; // for test chromecast two
+module.exports.scan = scanDevice; 
+module.exports.launch = launch; 
 
-startApp(ipTarget);
+// var ipTarget = '192.168.10.12'; // for test chromecast one
+// var ipTarget = '192.168.10.24'; // for test chromecast two
+
 
 // for run test 
 // $DEBUG=* node  screen.js
